f05f8e916c2035c166e78174c546e493b15c1ce6
leecc2015 <583388040@qq.com>
2019-01-06 17:03:51 +0800

    首页完成上下滑动

--- public/mobileli/js/index.js
+++ public/mobileli/js/index.js
@@ -6,4 +6,11 @@ $(function () {
         /*自动轮播图的间隔时间*/
         interval: 500
     });
+
+
+    // mui实现的选择器, 可以生成mui实例, 就可以调用 mui实例的方法
+  mui('.mui-scroll-wrapper').scroll({
+    deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    indicators: false //是否显示滚动条
+  });
 })
